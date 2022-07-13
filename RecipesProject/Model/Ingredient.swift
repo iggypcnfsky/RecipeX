80f7495fdb47ac98852b0b2e8e75f2a4daa3518f
Iggy <id@iggypacanowski.com>
2022-07-13 17:01:04 +0200

    recipe detail with picker for servings

--- RecipesProject/Model/Ingredient.swift
+++ RecipesProject/Model/Ingredient.swift
@@ -12,7 +12,7 @@ class Ingredient: Identifiable, Decodable {
     var id:UUID?
     var name:String
     var num:Int?
-    var denum:Int?
+    var denom:Int?
     var unit:String?
     
     
--- RecipesProject/Views/RecipeDetail.swift
+++ RecipesProject/Views/RecipeDetail.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct RecipeDetail: View {
     
+    @State var servingSize = 2
+    
     var recipe:Recipe
     
     var body: some View {
@@ -23,25 +25,46 @@ struct RecipeDetail: View {
                     .scaledToFill()
                     
                 
-                VStack(alignment: .leading) {
+                Text("Pick your serving size:")
+                    .font(.footnote)
+                    .padding(.top, 5)
+                    .padding(.horizontal, 10)
+                Picker("Pick your serving size", selection: $servingSize) {
+                    Text("2").tag(2)
+                    Text("4").tag(4)
+                    Text("6").tag(6)
+                    Text("8").tag(8)
+                }.pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 200)
+                    .padding(.horizontal, 10)
+                    .padding(.bottom, 10)
+                    
+                Divider().padding(.horizontal, 10)
+                
+                VStack(alignment: .leading, spacing: 4) {
                     Text("Ingredients")
-                        .font(.title2)
+                        .font(.title3)
                         .padding(.top, 5)
                         .padding(.bottom, 5)
                     
                     ForEach (recipe.ingredients) {item in
-                        Text(item.name)
+                        
+                        //here we can define changes to the ingredients based on the amount of servings.
+                        Text("- " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: servingSize) + " " + item.name)
+                            .font(.footnote)
+                        
+                        
                     }
                     
                 }.padding(.horizontal, 10)
                 
-                Divider()
+                Divider().padding(.horizontal, 10)
                 
                 //MARK: DIRECTIONS
                 VStack(alignment: .leading, spacing: 10){
                     ForEach(0..<recipe.directions.count, id: \.self) {r in
                         Text(String(r+1) + ". " + recipe.directions[r])
-                            .padding(.bottom, 10)
+                            .font(.footnote)
                             .padding(.top, 10)
                     }
                 }.padding(.horizontal, 10)
@@ -51,6 +74,7 @@ struct RecipeDetail: View {
             } //:MAIN VSTACK
 
         }.navigationBarTitle(recipe.name)
+            .ignoresSafeArea()
 
         
     }
@@ -61,6 +85,6 @@ struct RecipeDetail_Previews: PreviewProvider {
         
         let model = RecipeModel()
         
-        RecipeDetail(recipe: model.recipes[0])
+        RecipeDetail(recipe: model.recipes[1])
     }
 }
